17b3fc04e2a8080b8ef1648e8411a58c5cfe7ef0
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-08 12:22:13 -0600

    add card styling

--- Turing/mod-3/swapi-box/src/Card/Card.js
+++ Turing/mod-3/swapi-box/src/Card/Card.js
@@ -4,7 +4,7 @@ import './styles.css';
 const Card = ({individual}) => {
 
   if(individual.species) {
-        return <div> 
+        return <div className='card'> 
           <h2>Name: {individual.name}</h2>
           <p>Species: {individual.species}</p>
           <p>Homeworld: {individual.planetName}</p>
